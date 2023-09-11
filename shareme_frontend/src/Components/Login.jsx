2e636c47836c8b8bb945df2be0a454b2e492e7d7
cass <alicastilo1097@gmail.com>
2023-09-11 05:44:10 +0300

    start login

--- shareme_frontend/src/Components/Login.jsx
+++ shareme_frontend/src/Components/Login.jsx
@@ -2,13 +2,28 @@ import React from 'react'
 import GoogleLogin from "react-google-login";
 import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
+import ShareVideo from "../assets/share.mp4";
+import Logo from "../assets/logo.png";
+
+
 
 const Login = () => {
     return (
-        <div>
-            Login
+      <div className="flex justify-start items-center flex-col h-screan">
+        <div className="relative w-full h-full ">
+          <video
+            src={ShareVideo}
+            type="video/mp4"
+            loop
+            autoPlay
+            muted
+            controls={false}
+            // sx={{ width: "100%", height: "100%" , object: "cover" }}
+            className="w-full h-full object-cover"
+          ></video>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Login
